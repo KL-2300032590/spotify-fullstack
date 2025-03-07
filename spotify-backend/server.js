0fc9a382d4d765945beebb110162c802add7787d
PARDHASARADHI REDDY <2300032590@kluniversity.in>
2025-03-07 11:06:57 +0530

    Update server.js

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,38 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
 import songRouter from './src/routes/songRoute.js';
 import connectDB from './src/config/mongodb.js';
 import connectCloudinary from './src/config/cloudinary.js';
 import albumRouter from './src/routes/albumRoute.js';
 
-// App config
+// app config
 const app = express();
 const port = process.env.PORT || 4000;
-
-// Connect to database and cloudinary
 connectDB();
 connectCloudinary();
 
-// CORS setup to allow frontend to access the backend
-const allowedOrigins = [
-  'http://localhost:3000', // for local testing
-  'https://spotify-frontend-f16a.onrender.com' // production frontend
-];
 
-app.use(cors({
-  origin: allowedOrigins
-}));
-
-// Middlewares
+// middlewares 
 app.use(express.json());
+app.use(cors()); //allow fronted to connect with backend
 
-// Initialize routes
-app.use('/api/song', songRouter);
-app.use('/api/album', albumRouter);
 
-// Test route
-app.get('/', (req, res) => res.send('API WORKING'));
+//initilazing routes
+app.use("/api/song",songRouter);
+app.use("/api/album",albumRouter);
 
-// Start server
-app.listen(port, () => console.log(`Server Started on port ${port}`));
+app.get('/', (req,res)=> res.send("API WORKING"))
+
+app.listen(port , ()=>console.log(`Server Started on ${port}`));
